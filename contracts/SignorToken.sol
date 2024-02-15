f4f144cf436cfeb1f9d7202568f181fee75a7120
signor1 <emmanuelomemgboji@gmail.com>
2024-02-15 13:04:22 +0100

    feat: added mappings

--- contracts/SignorToken.sol
+++ contracts/SignorToken.sol
@@ -7,4 +7,10 @@ contract SignorToken {
     string tokenSymbol;
     uint256 totalSupply;
     address owner;
+
+    //making an amount tracable by address
+    mapping(address user => uint256) balances;
+    //making an amount tracable by address A and address A traced by address B
+    //key => (key => value)
+    mapping(address => mapping(address => uint256)) allow;
 }
